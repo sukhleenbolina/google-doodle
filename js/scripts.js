572231c60996fc90b383d9e646f5377504de5c39
Sukhleen <sbolina@eagles.ewu.edu>
2019-05-07 20:09:42 -0700

    Added gradient backgorund (random)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 console.log('OK');
+
+// Store Background Classes
+var bgs = ['one', 'two', 'three', 'four'];
+console.log(bgs);
+
+// Random Number Generator
+var max = 4;
+var randNum = Math.floor( Math.random() * max);
+console.log( randNum );
+
+// Change Background Class
+$('#stage').addClass( bgs[randNum] );
