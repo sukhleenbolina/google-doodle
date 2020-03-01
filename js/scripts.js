d32db858008aa0a8bbfca7b396a218bf80165d8a
Sukhleen Bolina <sbolina@eagles.ewu.edu>
2020-02-29 17:46:46 -0800

    Changed HTML CSS Javascript
    Theme is books, not Dali anymore

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,9 @@
-console.log('OK');
+$("#flashlight").bind('mousemove', function(e){
+	var offset = $('#flashlight').offset();
+	$('#flashlight-shine').css({
+		/*remove the border width + 1/2 the width + the offset of the base div to center the div on mousemove*/
+		left:  e.pageX - 3100 - offset.left,
+		top:   e.pageY - 3100 - offset.top,
+	});
+});
 
-
-
-var bgs = ['one', 'two', 'three', 'four'];
-console.log(bgs);
-
-
-var max = 4;
-var randNum = Math.floor( Math.random() * max);
-console.log( randNum );
-
-
-$('#stage').addClass( bgs[randNum] );
